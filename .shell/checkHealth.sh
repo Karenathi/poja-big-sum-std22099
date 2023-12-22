506fce6dee885f44cb962d38de90b3cf1e7c3a06
velvet_vixen <hei.karenandriantasy@gmail.com>
2023-12-22 09:29:26 +0300

    update: uate checkHealth.sh

--- .shell/checkHealth.sh
+++ .shell/checkHealth.sh
@@ -1,4 +1,13 @@
-sudo apt-get install jq
-export API_URL_SSM="`aws ssm get-parameter --name /poja-big-sum-std22099/$1/api/url`"
-export API_URL=`echo $API_URL_SSM | jq -r '.Parameter.Value'`
-curl --fail "$API_URL$2"
+#!/bin/bash
+
+ENV=$1
+ENDPOINT=$2
+
+AWS_REGION="eu-west-3"
+AWS_ACCESS_KEY_ID="your_access_key_id"
+AWS_SECRET_ACCESS_KEY="your_secret_access_key"
+sudo pacman -S aws-cli jq
+API_URL_SSM=$(aws ssm get-parameter --name "/poja-big-sum-std22099/$ENV/api/url" --region "$AWS_REGION" --output json)
+
+API_URL=$(echo "$API_URL_SSM" | jq -r '.Parameter.Value')
+curl --fail "$API_URL$ENDPOINT"
