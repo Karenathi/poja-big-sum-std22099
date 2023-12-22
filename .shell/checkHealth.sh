1ce7b93beefa42fc9843a9d9d83ef909c741e606
velvet_vixen <hei.karenandriantasy@gmail.com>
2023-12-22 09:32:25 +0300

    fix : fix checkHealth.sh

--- .shell/checkHealth.sh
+++ .shell/checkHealth.sh
@@ -4,8 +4,8 @@ ENV=$1
 ENDPOINT=$2
 
 AWS_REGION="eu-west-3"
-AWS_ACCESS_KEY_ID="your_access_key_id"
-AWS_SECRET_ACCESS_KEY="your_secret_access_key"
+AWS_ACCESS_KEY_ID="AKIA2YY5SSOHB5Y6KT6L"
+AWS_SECRET_ACCESS_KEY="IKYuWGiXFiQQ//hK2GBGTcgbmPo9bWoP3x5l3ygG"
 sudo pacman -S aws-cli jq
 API_URL_SSM=$(aws ssm get-parameter --name "/poja-big-sum-std22099/$ENV/api/url" --region "$AWS_REGION" --output json)
 
